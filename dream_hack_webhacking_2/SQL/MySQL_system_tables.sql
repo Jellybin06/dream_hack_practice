94fc5eb2dc577c5aee5b1c214255e8486d2b0492
Jellybin <pjbboseung0817@gmail.com>
2022-10-16 18:54:37 +0900

    22-10-16

--- dream_hack_webhacking_2/SQL/MySQL_system_tables.sql
+++ dream_hack_webhacking_2/SQL/MySQL_system_tables.sql
@@ -54,3 +54,65 @@ select TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME from information_schema.COLUMNS;
 */
 
 
+-- 실시간 실행 쿼리 정보
+-- PROCESSLIST 테이블을 통해 실시간으로 실행되는 쿼리를 조회할 수 있다.
+-- SYS 데이터베이스의 SESSION 테이블을 통해 실행 중인 계정과 함께 조회하는 방법도 있다.
+
+-- MySQL 실시간 실행 쿼리 조회
+select * from information_schema.PROCESSLIST;
+/*
++-------------------------------------------------+
+| info                                            |
++-------------------------------------------------+
+| select info from information_schema.PROCESSLIST |
++-------------------------------------------------+
+1 row in set (0.00 sec)
+*/
+
+
+-- MySQL 계정 및 실시간 실행 쿼리 조회
+select user,current_statement from sys.session;
+/*
++----------------+------------------------------------------------+
+| user           | current_statement                              |
++----------------+------------------------------------------------+
+| root@localhost | select user,current_statement from sys.session |
++----------------+------------------------------------------------+
+1 row in set (0.05 sec)
+*/
+
+
+-- USER_PRIVILEGES 테이블을 통해 MySQL 서버의 계정 정보를 조회할 수 있다
+-- USER_PRIVILEGES 테이블의 GRANTEE, PRIVILEGE_TYPE 그리고 IS_GRANTABLE을 조회하여 결과에 계정 정보와 각 권한에 대한 내용이 포함되게 할 수 있다.
+select GRANTEE,PRIVILEGE_TYPE,IS_GRANTABLE from information_schema.USER_PRIVILEGES; -- from 뒤로 -> infromation_schema 에있는 서버 계정 정보
+-- GRANTEE(수혜자? 계정 정보) PRIVILEGE_TYPE(특권) IS_GRANTABLE(부여 가능?)
+/*
++-------------------------+-------------------------+--------------+
+| GRANTEE                 | PRIVILEGE_TYPE          | IS_GRANTABLE |
++-------------------------+-------------------------+--------------+
+| 'root'@'localhost'      | SELECT                  | YES          |
+...
+| 'root'@'localhost'      | SUPER                   | YES          |
+...
+| 'user_test'@'localhost' | USAGE                   | NO           |
++-------------------------+-------------------------+--------------+
+58 rows in set (0.00 sec)
+*/
+
+
+select User, authentication_string from mysql.user;
+/*
++------------------+-------------------------------------------+
+| User             | authentication_string                     |
++------------------+-------------------------------------------+
+| root             | *...                                      |
+| mysql.sys        | *THISISNOTAVALIDPASSWORDTHATCANBEUSEDHERE |
+| mysql.session    | *THISISNOTAVALIDPASSWORDTHATCANBEUSEDHERE |
+| user_test        | *...                                      |
++------------------+-------------------------------------------+
+4 rows in set (0.00 sec)
+*/
+
+
+
+
